47dc4e6b3a149839b15b20becbe2169166fd40bc
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-06 12:30:33 +0600

    signIn and signOut implemented

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 
 import './App.css';
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+
+  const googleProvider = new GoogleAuthProvider();
 
   const hadleGoogleSignIn = () => {
-    console.log('sign in');
+    signInWithPopup(auth, googleProvider)
+    .then(result => {
+      const user = result.user;
+      setUser(user);
+      console.log(user)
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      setUser({});
+    })
+    .catch(error => {
+      setUser({});
+    })
   }
 
   return (
     <div className="App">
       <button onClick={hadleGoogleSignIn}>Sign in With Google</button>
+      <button onClick={handleSignOut}>Sign Out</button>
+      <div>
+        <h3>{user.displayName}</h3>
+        <h5>{user.email}</h5>
+        <img src={user.photoURL} alt="" />
+      </div>
     </div>
   );
 }
