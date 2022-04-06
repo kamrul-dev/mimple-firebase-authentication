5daf3bca30e4ce2d401f25f787a2c2f5d0be9e8e
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-06 11:43:52 +0600

    import getAuth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import { getAuth } from "firebase/auth";
+import app from './firebase.init';
+
+const auth = getAuth(app);
 
 function App() {
+
+  const hadleGoogleSignIn = () => {
+    console.log('sign in');
+  }
+
   return (
     <div className="App">
-      <button>Sign in With Google</button>
+      <button onClick={hadleGoogleSignIn}>Sign in With Google</button>
     </div>
   );
 }
